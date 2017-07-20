a024ef4eb32343c6bedb5cd8bb35fbc10d9e74a7
Amber Jain <i.amber.jain@gmail.com>
2017-07-20 20:30:34 +0530

    Models for tracks app created and migration is added.

--- tracks/models.py
+++ tracks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Genres(models.Model):
+    """Model to store all genres"""
+    genre = models.CharField(max_length=30, unique=True, blank=False)
+
+
+class Track(models.Model):
+    """Model to store track of music (Took assumtion that title won't
+    be bigger then 200 chars)"""
+    title = models.CharField(max_length=200, unique=True, blank=False)
+    genre = models.ForeignKey(Genres, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
