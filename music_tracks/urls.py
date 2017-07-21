53281d243c417947017cdb02890ce33398401e83
Amber Jain <i.amber.jain@gmail.com>
2017-07-21 18:59:39 +0530

    Url pattarn changed

--- music_tracks/urls.py
+++ music_tracks/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^tracks/', include('tracks.urls')),
+    url(r'^', include('tracks.urls'), name='music-tracks'),
 ]
