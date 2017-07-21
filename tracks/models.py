5301c780ecff616b90d425daab17bda3a14a28bf
Amber Jain <i.amber.jain@gmail.com>
2017-07-21 23:11:28 +0530

    String method added to model.

--- tracks/models.py
+++ tracks/models.py
@@ -5,6 +5,9 @@ class Genres(models.Model):
     """Model to store all genres"""
     genre = models.CharField(max_length=30, unique=True, blank=False)
 
+    def __str__(self):
+        return self.genre
+
 
 class Track(models.Model):
     """Model to store track of music (Took assumtion that title won't
@@ -12,4 +15,7 @@ class Track(models.Model):
     title = models.CharField(max_length=200, unique=True, blank=False)
     genre = models.ForeignKey(Genres, on_delete=models.CASCADE)
     rating = models.IntegerField()
+
+    def __str__(self):
+        return self.title
 
