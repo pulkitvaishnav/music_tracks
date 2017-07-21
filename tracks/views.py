1161c472a0f7cf92cff78b545e63f17666982485
Amber Jain <i.amber.jain@gmail.com>
2017-07-21 16:34:12 +0530

    Views added for adding genre and tracks

--- tracks/views.py
+++ tracks/views.py
@@ -1,3 +1,27 @@
+from django import forms
 from django.shortcuts import render
+from .models import Track
+from .forms import TrackForm, GenresForm
 
-# Create your views here.
+def add_track(request):
+    if request.method == 'POST':
+        form = TrackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # return redirect('tracks-add')
+    else:
+        form = TrackForm
+        
+    return render(request, 'tracks/track_form.html', {'form': form})
+
+
+def add_genres(request):
+    if request.method == 'POST':
+        form = GenresForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # return redirect('tracks-add')
+    else:
+        form = GenresForm
+        
+    return render(request, 'genres/genre_form.html', {'form': form})
