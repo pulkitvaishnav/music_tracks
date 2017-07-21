0afdf8b92a71eb890e66008ac0ef34227eb08bde
Amber Jain <i.amber.jain@gmail.com>
2017-07-21 23:15:39 +0530

    Urls for tracks_list and tracks_detail is added.

--- tracks/urls.py
+++ tracks/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^$', views.track, name='tracks'),
+    url(r'^tracks/$', views.track_list, name='track_list'),
+    url(r'^track/(?P<track_id>[0-9]+)/$', views.track_detail, name='track_detail'),
     url(r'track/add/$', views.add_track, name='tracks-add'),
-    url(r'genre/add/$', views.add_genres, name='genres-add')
+    url(r'genre/add/$', views.add_genres, name='genres-add'),
 ]
