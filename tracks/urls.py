0e7d622ca2a18945acd32403f51a47bd6d01410b
pulkitvaishnav <pulkitvaishnav35@gmail.com>
2017-07-24 04:12:50 +0530

    Urls added to edit tracks by using same add_track view for edit

--- tracks/urls.py
+++ tracks/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'Track'
 urlpatterns = [
     url(r'^tracks/$', views.track_list, name='track_list'),
     url(r'^track/(?P<track_id>[0-9]+)/$', views.track_detail, name='track_detail'),
     url(r'^genres/$', views.genre_list, name='genre_list'),
     url(r'^genre/(?P<genres_id>[0-9]+)/$', views.genre_detail, name='genre_detail'),
-    url(r'track/add/$', views.add_track, name='tracks-add'),
-    url(r'genre/add/$', views.add_genres, name='genres-add'),
+    url(r'^track/add/$', views.add_track, name='tracks-add'),
+    url(r'^genre/add/$', views.add_genres, name='genres-add'),
+    url(r'^track/(?P<pk>[0-9]+)/edit/$', views.add_track, name='tracks-edit'),
 ]
