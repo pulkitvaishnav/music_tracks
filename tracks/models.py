b65b0c0daef257776684b8e9b35f57ba61ae5f0c
pulkitvaishnav <pulkitvaishnav35@gmail.com>
2017-07-24 05:40:27 +0530

    Related name added to reversly track the model. That is to track values of 'track' from genre relation

--- tracks/models.py
+++ tracks/models.py
@@ -13,7 +13,8 @@ class Track(models.Model):
     """Model to store track of music (Took assumtion that title won't
     be bigger then 200 chars)"""
     title = models.CharField(max_length=200, unique=True, blank=False)
-    genre = models.ForeignKey(Genres, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genres, on_delete=models.CASCADE,
+                              related_name='track')
     rating = models.IntegerField()
 
     def __str__(self):
