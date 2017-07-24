bea9785ab924ef021574c167bf86e98a0adcf88f
pulkitvaishnav <pulkitvaishnav35@gmail.com>
2017-07-24 05:39:55 +0530

    Input box replaced by radio buttons in form

--- tracks/forms.py
+++ tracks/forms.py
@@ -1,14 +1,20 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Track, Genres
 
 
-class GenresForm(ModelForm):
+class GenresForm(forms.ModelForm):
+
     class Meta:
         model = Genres
         fields = ['genre']
 
 
-class TrackForm(ModelForm):
+class TrackForm(forms.ModelForm):
+    
     class Meta:
         model = Track
         fields = ['title', 'genre', 'rating']
+
+    RATING_CHOICES = ((5, '5'), (4, '4'), (3, '3'), (2, '2'), (1, '1'))
+    rating = forms.TypedChoiceField(choices=RATING_CHOICES,
+    								widget=forms.RadioSelect, coerce=int)
