b95942d651d0b273df2985ceb651b4aac8943452
pulkitvaishnav <pulkitvaishnav35@gmail.com>
2017-07-24 06:42:02 +0530

    COmments added for better code readability

--- tracks/forms.py
+++ tracks/forms.py
@@ -3,6 +3,7 @@ from .models import Track, Genres
 
 
 class GenresForm(forms.ModelForm):
+    """Form for genre"""
 
     class Meta:
         model = Genres
@@ -10,11 +11,12 @@ class GenresForm(forms.ModelForm):
 
 
 class TrackForm(forms.ModelForm):
+    """Form to add a track"""
     
     class Meta:
         model = Track
         fields = ['title', 'genre', 'rating']
-
+    # Customized the rating field so that one can add only 5,4 .. 1 only
     RATING_CHOICES = ((5, '5'), (4, '4'), (3, '3'), (2, '2'), (1, '1'))
     rating = forms.TypedChoiceField(choices=RATING_CHOICES,
-    								widget=forms.RadioSelect, coerce=int)
+                                    widget=forms.RadioSelect, coerce=int)
--- tracks/views.py
+++ tracks/views.py
@@ -7,12 +7,16 @@ from .forms import TrackForm, GenresForm
 
 
 def track_list(request):
+    """View to search on the basis of either music title or genre.
+    If no filter is provided it return all tracks."""
     search_word = request.GET.get('search_box')
     if search_word:
+        # Query to find all tracks which satisfy the above search term
         track_list = Track.objects.filter(Q(title__icontains=search_word) |
                                           Q(genre__genre__icontains=search_word))
     else:
         track_list = Track.objects.all()
+    # Pagination tool to show only 10 tracks per page
     paginator = Paginator(track_list, 10)
     page = request.GET.get('page')
     try:
@@ -26,17 +30,22 @@ def track_list(request):
 
 
 def track_detail(request, track_id):
+    """View to return the individual track details"""
     track = get_object_or_404(Track, pk=track_id)
     return render(request, 'tracks/track_detail.html', {'track': track})
 
 
 def add_track(request, pk=None):
+    """View to add and edit any track"""
     try:
         instance = Track.objects.get(pk=pk)
-    except :
+    except Track.DoesNotExist:
+        # if track is added for first time we add it to db.
         form = TrackForm(request.POST)
     else:
+        # if app is already exist we edit that
         form = TrackForm(request.POST or None, instance=instance)
+    # If requsted to edit
     if request.method == 'POST':
         if form.is_valid():
             instance = form.save(commit=False)
@@ -46,6 +55,7 @@ def add_track(request, pk=None):
 
 
 def genre_list(request):
+    """View to list all genre and paginated for 10 page per page."""
     genre_list = Genres.objects.all()
     paginator = Paginator(genre_list, 10)
     page = request.GET.get('page')
@@ -60,12 +70,14 @@ def genre_list(request):
 
 
 def genre_detail(request, genres_id):
+    """View to return an individual genre detail with all music tracks of it."""
     genres = get_object_or_404(Genres, pk=genres_id)
     return render(request, 'genres/genre_detail.html', {'genre': genres.genre,
                                                         'genre_track': genres.track.all()})
 
 
 def add_genres(request):
+    """View to add and edit any genre."""
     if request.method == 'POST':
         form = GenresForm(request.POST)
         if form.is_valid():
